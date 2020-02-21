69c3d9e5211638946313e37e858d0fa49fbd91f3
PraveenC <praveenkumar7501@gmail.com>
2020-02-21 21:15:34 +0530

    21-feb-20

---
+++ build/precache-manifest.ba15927298d301315e10577b2a96b704.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "877d55795c51ad4de128",
+    "url": "/static/js/main.877d5579.chunk.js"
+  },
+  {
+    "revision": "d2a72ad7e0b312479945",
+    "url": "/static/js/1.d2a72ad7.chunk.js"
+  },
+  {
+    "revision": "877d55795c51ad4de128",
+    "url": "/static/css/main.1be6b69d.chunk.css"
+  },
+  {
+    "revision": "6f9172a6985c43f5eaad6933eb02ed89",
+    "url": "/index.html"
+  }
+];
---
+++ src/Input.js
@@ -0,0 +1,36 @@
+import {Component} from "react";
+import React from "react";
+
+class Input extends Component {
+  state = {
+    text: ""
+  }
+
+  onChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({text: ""});
+    this.props.onSendMessage(this.state.text);
+  }
+
+  render() {
+    return (
+      <div className="Input container">
+        <form onSubmit={e => this.onSubmit(e)}>
+          <input
+            onChange={e => this.onChange(e)}
+            value={this.state.text}
+            type="text"
+            placeholder="Type what you want to share..."
+          />
+          <button> <span className="hidden-sm hidden-xs">Send</span> <i className="fa fa-paper-plane-o"></i></button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Input;
